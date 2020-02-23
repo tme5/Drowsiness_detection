7e15a0e9826eddaba876031bc7dc20c9c4f4176a
Tushar Mangale <tushar.mangale@thalesesecurity.com>
2020-02-23 20:21:01 +0530

    Changes in alarm

--- detector.py
+++ detector.py
@@ -4,6 +4,7 @@ from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
 from threading import Thread
+import matplotlib.pyplot as plt
 import numpy as np
 import argparse
 import imutils
@@ -11,24 +12,30 @@ import time
 import dlib
 import cv2
 import os
+from gtts import gTTS
 
 class BaseDetection():
     def __init__(self):
-        pass
+        self.yawn_alarm_msg = 'take some fresh air sir'
+        self.eye_alarm_msg = 'wake up sir'
+        self.yawn_alarm_file = 'yawn_alarm.mp3'
+        self.eye_alarm_file = 'eye_alarm.mp3'
+        tts = gTTS(text=self.yawn_alarm_msg, lang='en')
+        tts.save(self.yawn_alarm_file)
+        tts = gTTS(text=self.eye_alarm_msg, lang='en')
+        tts.save(self.eye_alarm_file)
     
     def alarm(self, msg):
         while self.eye_alarm:
-            print('call')
-            s = 'espeak "'+msg+'"'
-            os.system(s)
-
-        if yawn_alarm:
-            print('call')
+            print('[ALARM] Sleeping Alert')
+            os.system("mpg321 %s" %self.eye_alarm_file)
+            
+        if self.yawn_alarm:
+            print('[WARNING] Drowsiness Alert')
             self.saying = True
-            s = 'espeak "' + msg + '"'
-            os.system(s)
+            os.system("mpg321 %s" %self.yawn_alarm_file)
             self.saying = False
-
+            
     def eye_aspect_ratio(self, eye):
         A = dist.euclidean(eye[1], eye[5])
         B = dist.euclidean(eye[2], eye[4])
@@ -83,10 +90,20 @@ class MyDectection(BaseDetection):
     def run_dectection(self):
         while True:
             _frame = self.vs.read()
-            self.frame = imutils.resize(_frame, width=450)
+            self.frame = imutils.resize(_frame, width=700)
             #cv2.namedWindow('IMG')
             gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-            rects = self.detector(gray, 0)
+            
+            # Try
+            #img_hsv = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
+            #mask = ((img_hsv > np.array([0, 0, 230])).astype(np.float32) + (img_hsv > np.array([0, 0, 230])).astype(np.float32) * (-0.5) + 0.5)
+            #img_partly_darken = cv2.cvtColor(mask * img_hsv, cv2.COLOR_HSV2BGR)
+            
+            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+            cl1 = clahe.apply(gray)
+            
+            rects = self.detector(cl1, 0)
+            
             #rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
             #    	minNeighbors=5, minSize=(30, 30),
             #    	flags=cv2.CASCADE_SCALE_IMAGE)
@@ -123,9 +140,9 @@ class MyDectection(BaseDetection):
                         if self.eye_alarm == False:
                             self.eye_alarm = True
                             print('wake up sir')
-                            #t = Thread(target=self.alarm, args=('wake up sir',))
-                            #t.deamon = True
-                            #t.start()
+                            t = Thread(target=self.alarm, args=('wake up sir',))
+                            t.deamon = True
+                            t.start()
             
                         cv2.putText(self.frame, "DROWSINESS ALERT!", (10, 30),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
@@ -135,14 +152,14 @@ class MyDectection(BaseDetection):
                     self.eye_alarm = False
             
                 if (yawn_index > self.YAWN_THRESH):
-                        cv2.putText(self.frame, "Yawn Alert", (10, 30),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                        if self.yawn_alarm == False and self.saying == False:
-                            self.yawn_alarm = True
-                            print('take some fresh air sir')
-                            #t = Thread(target=self.alarm, args=('take some fresh air sir',))
-                            #t.deamon = True
-                            #t.start()
+                    cv2.putText(self.frame, "Yawn Alert", (10, 30),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    if self.yawn_alarm == False and self.saying == False:
+                        self.yawn_alarm = True
+                        print('take some fresh air sir')
+                        t = Thread(target=self.alarm, args=('take some fresh air sir',))
+                        t.deamon = True
+                        t.start()
                 else:
                     self.yawn_alarm = False
             
