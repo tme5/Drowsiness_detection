49a9cf25b1d6d8e6b486f8e3c64b8533ae349e3a
Tushar Mangale <tushar.mangale@thalesesecurity.com>
2020-02-25 15:02:00 +0530

    Changes to get face of interest and save output

--- sqlite_db.py
+++ sqlite_db.py
@@ -6,18 +6,25 @@ class SqliteDB():
         self.conn = sqlite3.connect('detection.db')
         self.cursor = self.conn.cursor()
         self.cursor.execute("DROP TABLE IF EXISTS detection_data")
-                                
-    def insert_data(self, counter, EAR, YAWN):
+
+    def insert_data(self, counter, EAR, EAR_INV, YAWN):
         with self.conn:
-            self.cursor.execute("CREATE TABLE IF NOT EXISTS detection_data (counter integer, EAR real, YAWN real)")
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS detection_data (counter real, EAR real,"
+                                " EAR_INV real, YAWN real)")
             self.cursor.execute("DELETE FROM detection_data WHERE counter<:limit", {'limit': counter-900})
-            self.cursor.execute("INSERT INTO detection_data VALUES (:counter, :EAR, :YAWN)", {'counter': counter, 'EAR': EAR, 'YAWN': YAWN})
-    
+            self.cursor.execute("INSERT INTO detection_data VALUES (:counter, :EAR, :EAR_INV, :YAWN)",
+                                {'counter': counter, 'EAR': EAR, 'EAR_INV': EAR_INV, 'YAWN': YAWN})
+
+    def insert_blink_width(self, time, bw):
+        with self.conn:
+            self.cursor.execute("CREATE TABLE IF NOT EXISTS blink_width (time real, bw real)")
+            self.cursor.execute("INSERT INTO blink_width VALUES (:time, :bw)", {'time': time, 'bw': bw})
+
     def get_all_data(self):
         counter = time.time()- 900
         self.cursor.execute("SELECT * FROM detection_data WHERE counter>=:counter", {'counter': counter})
         return self.cursor.fetchall()
-    
+
     def get_yawn_th(self, YAWN_TH):
         self.cursor.execute("SELECT * FROM detection_data WHERE YAWN=:YAWN_TH", {'YAWN_TH': YAWN_TH})
         return self.cursor.fetchall()
